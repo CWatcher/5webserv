b3bc87afe0d5ace4410d53a440ea189d8ad0cdcc
Andrey Krutikhin <54722864+Krutix@users.noreply.github.com>
2022-02-25 20:29:37 +0300

    Добавление системы сборки и базового Makefile (#1)
    * inital version of makefile
    
    * simple compilable cpp file for makefile test
    
    * Fix: bug with cicle variable init
    
    * upgrade base Makefile
    
    * More general rule for addition fclean
    
    * fix bugs and add debug sans
    
    * proper exe dependency
    
    * Change compiler to \"c++\"
    
    * code cleanup. new way too build projects 'BUILD=release make'
    
    * new line at end of tm.mk file
    
    * remove brake debug rules
    
    * fix include dependency bug

---
+++ Makefile
@@ -0,0 +1,44 @@
+include make/tm.mk
+
+PROJECT_NAME  = webserv
+NAME          = webserv
+
+CXX           = c++
+LD            = c++
+CXXFLAGS      = -Wall -Wextra -Werror
+
+BUILD ?= debug
+
+$(call add/project,$(PROJECT_NAME))
+$(PROJECT_NAME)_SRCS     += srcs/main.cpp
+$(PROJECT_NAME)_INC      += ./
+$(PROJECT_NAME)_CXXFLAGS += --std=c++98
+$(PROJECT_NAME)_C_DEP    += ./Makefile
+$(PROJECT_NAME)_LD_DEP   += ./Makefile
+
+
+$(call add/subproj,$(PROJECT_NAME),debugbase)
+debugbase_CXXFLAGS       += -O0 -g3
+
+$(call add/subproj,$(PROJECT_NAME),debug)
+DEBUG_SANS =    -fsanitize=address \
+                -fsanitize=undefined
+debug_CXXFLAGS       += $(DEBUG_SANS)
+debug_LDFLAGS        += $(DEBUG_SANS)
+$(call add/exe,debug,$(NAME))
+
+$(call add/subproj,$(PROJECT_NAME),release)
+release_CXXFLAGS     += -O2
+$(call add/exe,release,$(NAME))
+
+.PHONY: all clean fclean re
+
+$(NAME): $($(BUILD)_EXE)
+	cp $($(BUILD)_EXE) $(NAME)
+all: $(NAME)
+$(BUILD)_BUILD  += $(NAME)
+$(BUILD)_FCLEAN += $(NAME)
+
+clean: $(BUILD)/clean
+fclean: $(BUILD)/fclean
+re: $(BUILD)/re
---
+++ make/tm.mk
@@ -0,0 +1,181 @@
+.DEFAULT_GOAL  := all
+.SECONDEXPANSION:
+# External programms
+PYTHON          = python3
+NORMINETTE      = norminette
+RM              = rm -rf
+AR              = ar rcs
+MKDIR           = mkdir -p
+
+# Pretty print
+colored        = 1
+ifeq ($(colored), 1)
+color_RED      = \x1b[31m
+color_GREEN    = \x1b[32m
+color_YELLOW   = \x1b[33m
+color_BLUE     = \x1b[34m
+color_MAGENTA  = \x1b[35m
+color_CYAN     = \x1b[36m
+color_RESET    = \x1b[0m
+reset_line     = \033[A\033[2K
+endif
+
+pretty_done     = [$(color_GREEN)✓$(color_RESET)]
+pretty_fail     = [$(color_RED)✕$(color_RESET)]
+
+empty           =
+space           = $(empty) $(empty)
+comma           = $(empty),$(empty)
+
+#######################################################
+#
+# Real build
+#
+
+TARGET_OS = $(shell uname)
+#ifeq ($(TARGET_OS), Linux)
+#endif
+#ifeq ($(TARGET_OS), Darwin)
+#endif
+
+define __add/project =
+$(1)_CC         = $(CC)
+$(1)_CXX        = $(CXX)
+$(1)_CFLAGS     = $(CFLAGS)
+$(1)_CXXFLAGS   = $(CXXFLAGS)
+$(1)_INC        =
+$(1)_FINC       = $$($(1)_INC:%=-I%)
+$(1)_LD         = $(LD)
+$(1)_LDFLAGS    = $(LDFLAGS)
+$(1)_LIBS       = $(LIBS)
+
+$(1)_C_DEP      =
+$(1)_LD_DEP     =
+$(1)_CLEAN_DEP  =
+$(1)_FCLEAN_DEP =
+
+$(1)_BUILD      =
+$(1)_SRCS       =
+$(1)_BDIR       = .$(1)/
+$(1)_OBJS       = $$($(1)_SRCS:%.cpp=$$($(1)_BDIR)%.o)
+$(1)_DFILES     = $$($(1)_SRCS:%.cpp=$$($(1)_BDIR)%.d)
+
+$(1)_CLEAN      = 
+$(1)_FCLEAN     = $$($(1)_CLEAN)
+
+$(call __add/build_dir,$(1))
+$(call __add/objrule,$(1))
+$(call __add/baserules,$(1))
+endef
+
+define __add/subproj =
+$(call __add/project,$(2))
+$(2)_CC        = $$($(1)_CC)
+$(2)_CXX       = $$($(1)_CXX)
+$(2)_CFLAGS    = $$($(1)_CFLAGS)
+$(2)_CXXFLAGS  = $$($(1)_CXXFLAGS)
+$(2)_INC       = $$($(1)_INC)
+$(2)_LD        = $$($(1)_LD)
+$(2)_LDFLAGS   = $$($(1)_LDFLAGS)
+$(2)_LIBS      = $$($(1)_LIBS)
+$(2)_SRCS      = $$($(1)_SRCS)
+
+$(2)_C_DEP      = $$($(1)_C_DEP)
+$(2)_LD_DEP     = $$($(1)_LD_DEP)
+$(2)_CLEAN_DEP  = $$($(1)_CLEAN_DEP)
+$(2)_FCLEAN_DEP = $$($(1)_FCLEAN_DEP)
+
+$(2)_CLEAN      = $$($(1)_CLEAN)
+$(2)_FCLEAN     = $$($(1)_FCLEAN)
+endef
+
+# auto build directories
+# root src files must start with "./"
+define __add/build_dir =
+.PRECIOUS: $$($(1)_BDIR)/. $$($(1)_BDIR)%/.
+$$($(1)_BDIR)/.:
+	mkdir -p $$@
+$$($(1)_BDIR)%/.:
+	mkdir -p $$@
+endef
+
+define __add/objrule =
+$$($(1)_BDIR)%.o:	%.cpp $$($(1)_C_DEP) | $$$$(@D)/.
+	$$($(1)_CXX) $$($(1)_CXXFLAGS) $$($(1)_FINC)  -c $$< -o $$@ -MD
+endef
+
+#! @1 - project name
+#! @2 - out name
+define __add/exe =
+$(1)_EXE = $$($(1)_BDIR)/$(2)
+$(1)_BUILD += $$($(1)_EXE)
+-include $$($(1)_DFILES)
+
+$$($(1)_EXE): $$($(1)_OBJS) $$($(1)_DEPEND)$$($(1)_LD_DEP)
+	$$($(1)_LD) $$($(1)_LDFLAGS) $$($(1)_OBJS) -o $$($(1)_EXE) $$($(1)_LIBS)
+
+.PHONY: $(1)/exe
+$(1)/exe: $$($(1)_EXE)
+
+$(1)_FCLEAN  += $$($(1)_EXE)
+endef
+
+#! @1 - project name
+#! @2 - out name
+define __add/lib =
+$(1)_LIB = $$($(1)_BDIR)/$(2)
+$(1)_BUILD += $$($(1)_LIB)
+-include $$($(1)_DFILES)
+
+$$($(1)_LIB): $$($(1)_OBJS) $$($(1)_DEPEND) $$($(1)_LD_DEP)
+	$(AR) $$($(1)_LIB) $$($(1)_OBJS)
+
+.PHONY: $(1)/lib
+$(1)/lib: $$($(1)_LIB)
+
+$(1)_FCLEAN  += $$($(1)_LIB)
+endef
+
+#! @1 - project name
+#! @2 - out name
+define __add/shlib =
+$(1)_SHLIB = $$($(1)_BDIR)/$(2)
+$(1)_BUILD += $$($(1)_SHLIB)
+-include $$($(1)_DFILES)
+
+$$($(1)_SHLIB): $$($(1)_OBJS) $$($(1)_DEPEND)) $$($(1)_LD_DEP)
+	$$($(1)_LD) $$($(1)_LDFLAGS) -shared -Wl,-soname,$(2) -o $$($(1)_SHLIB) $$($(1)_OBJS) $$($(1)_LIBS)
+
+.PHONY: $(1)/shlib
+$(1)/shlib: $$($(1)_SHLIB)
+
+$(1)_FCLEAN       += $$($(1)_SHLIB)
+endef
+
+define __add/baserules =
+.PHONY: $(1)/clean
+$(1)/clean: $$($(1)_CLEAN_DEP)
+	-$(RM) $$($(1)_CLEAN) $$($(1)_OBJS) $$($(1)_DFILES)
+
+.PHONY: $(1)/fclean
+$(1)/fclean: $$($(1)_FCLEAN_DEP)
+	-$(RM) $$($(1)_FCLEAN) $$($(1)_OBJS) $$($(1)_DFILES) $$($(1)_BDIR)
+
+.PHONY: $(1)/re
+$(1)/re:
+	+$(MAKE) $(1)/fclean
+	+$(MAKE) $$($(1)_BUILD)
+endef
+
+#! @1 - project name
+add/project = $(eval $(call __add/project,$(1),$(2)))
+
+#! @1 - project name
+#! @2 - sub project name
+add/subproj = $(eval $(call __add/subproj,$(1),$(2)))
+
+#! @1 - project name
+#! @2 - out name
+add/exe = $(eval $(call __add/exe,$(1),$(2)))
+add/lib = $(eval $(call __add/lib,$(1),$(2)))
+add/shlib = $(eval $(call __add/shlib,$(1),$(2)))
